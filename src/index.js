d19b17e06f83b5a8c0d5614f2eaf1f3c16555f1d
Azeez Saka <saoconcept1@gmail.com>
2024-06-24 09:40:59 -0700

    Modified src/index.js, src/index.scss, and  package.json files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./contextAPI/StoreContextAPI.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
 );
