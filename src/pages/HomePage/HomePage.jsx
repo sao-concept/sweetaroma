922027b979e89f95662c4ae1988066e4cefa2c24
Azeez Saka <saoconcept1@gmail.com>
2024-06-26 07:45:54 -0700

    Added src/pages/HomePage feature

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./HomePage.scss";
+import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
+import ExploreMenuComponent from "../../components/ExploreMenuComponent/ExploreMenuComponent";
+// import FoodDisplayComponent from "../../components/FoodDisplayComponent/FoodDisplayComponent";
+// import AppDownloadComponent from "../../components/AppDownloadComponent/AppDownloadComponent";
+
+function HomePage() {
+  const [categorylist, setCategorylist] = useState("All");
+
+  return (
+    <div>
+      <HeaderComponent />
+      <ExploreMenuComponent
+        categorylist={categorylist}
+        setCategorylist={setCategorylist}
+      />
+      {/* <FoodDisplayComponent categorylist={categorylist} />
+      <AppDownloadComponent /> */}
+    </div>
+  );
+}
+
+export default HomePage;
