846747ddbdffb7211622296b56ea2aaa7687e239
Azeez Saka <saoconcept1@gmail.com>
2024-06-26 07:23:27 -0700

    Modified src/components/App/App.jsx file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
 import "./App.scss";
 import NavbarComponent from "../NavbarComponent/NavbarComponent";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavbarComponent
-        setShowLoginPage={setShowLoginPage}
-        setUserData={setUserData}
-      />
+      <NavbarComponent />
+      {/* <Routes>
+        <Route path="/" element={<HomePage userData={userData} />} />
+        <Route path="/cartpage" element={<CartPage />} />
+        <Route path="/placeorderpage" element={<PlaceOrderPage />} />
+      </Routes> */}
     </div>
   );
 }
