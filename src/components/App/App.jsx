786f20ee2900a8f9116da70242fa7f630ab8f01b
Azeez Saka <saoconcept1@gmail.com>
2024-06-27 12:33:21 -0700

    Modified src/components/App/App.jsx file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,21 @@
 import "./App.scss";
 import NavbarComponent from "../NavbarComponent/NavbarComponent";
 import { Route, Routes } from "react-router-dom";
+import FooterComponent from "../FooterComponent/FooterComponent";
 
 function App() {
   return (
-    <div className="App">
-      <NavbarComponent />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cartpage" element={<CartPage />} />
-        <Route path="/placeorderpage" element={<PlaceOrderPage />} />
-      </Routes>
-    </div>
+    <>
+      <div className="App">
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cartpage" element={<CartPage />} />
+          <Route path="/placeorderpage" element={<PlaceOrderPage />} />
+        </Routes>
+      </div>
+      <FooterComponent />
+    </>
   );
 }
 
