67f41027459530459a1b9e6c3849d6d772fc9d33
Azeez Saka <saoconcept1@gmail.com>
2024-06-30 12:48:54 -0700

    Modified src/components/App/App.jsx file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavbarComponent from "../NavbarComponent/NavbarComponent";
 import HomePage from "../../pages/HomePage/HomePage";
@@ -12,59 +12,59 @@ import { AuthProvider } from "../../contextAPI/AuthContext";
 import PurchaseHistoryProviderComponent from "../PurchaseHistoryProviderComponent/PurchaseHistoryProviderComponent";
 import PurchaseHistoryPage from "../../pages/PurchaseHistoryPage/PurchaseHistoryPage";
 import PaystackPayment from "../../pages/PaystackPayment/PaystackPayment";
-import { StoreContext } from "../../contextAPI/StoreContextAPI";
-import { useContext } from "react";
+import StoreContextProvider, {
+  StoreContext,
+} from "../../contextAPI/StoreContextAPI";
 
 function App() {
   const [showLoginPage, setShowLoginPage] = useState(false);
   const [userData, setUserData] = useState(null);
-
   const { getTotalCartCost } = useContext(StoreContext);
 
   return (
     <AuthProvider>
-      {showLoginPage && (
-        <LoginPopupComponent
-          setShowLoginPage={setShowLoginPage}
-          setUserData={setUserData}
-        />
-      )}
-      <div className="app">
-        <NavbarComponent
-          setShowLoginPage={setShowLoginPage}
-          setUserData={setUserData}
-        />
-        {userData && (
-          <>
-            <WelcomeMessageComponent userData={userData} />
-          </>
+      <StoreContextProvider>
+        {showLoginPage && (
+          <LoginPopupComponent
+            setShowLoginPage={setShowLoginPage}
+            setUserData={setUserData}
+          />
         )}
-        <div className="main-content">
-          <Routes>
-            <Route path="/" element={<HomePage userData={userData} />} />
-            <Route path="/cartpage" element={<CartPage />} />
-            <Route path="/placeorderpage" element={<PlaceOrderPage />} />
-
-            <Route
-              path="/purchase-history"
-              element={
-                <PurchaseHistoryProviderComponent>
-                  <PurchaseHistoryPage />
-                </PurchaseHistoryProviderComponent>
-              }
-            />
-
-            <Route
-              path="/paystack-payment"
-              element={
-                <PaystackPayment getTotalCartCost={getTotalCartCost()} />
-              }
-            />
-          </Routes>
+        <div className="app">
+          <NavbarComponent
+            setShowLoginPage={setShowLoginPage}
+            setUserData={setUserData}
+          />
+          {userData && (
+            <>
+              <WelcomeMessageComponent userData={userData} />
+            </>
+          )}
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<HomePage userData={userData} />} />
+              <Route path="/cartpage" element={<CartPage />} />
+              <Route path="/placeorderpage" element={<PlaceOrderPage />} />
+              <Route
+                path="/purchase-history"
+                element={
+                  <PurchaseHistoryProviderComponent>
+                    <PurchaseHistoryPage />
+                  </PurchaseHistoryProviderComponent>
+                }
+              />
+              <Route
+                path="/paystack-payment"
+                element={
+                  <PaystackPayment getTotalCartCost={getTotalCartCost()} />
+                }
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
-      <ScrollToTopComponent />
-      <FooterComponent />
+        <ScrollToTopComponent />
+        <FooterComponent />
+      </StoreContextProvider>
     </AuthProvider>
   );
 }
