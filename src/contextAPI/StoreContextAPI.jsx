b4f0ae6d0ff24f7f8bc398cf1d132efe35668e3f
Azeez Saka <saoconcept1@gmail.com>
2024-06-30 12:36:39 -0700

    Modified src/contextAPI/StoreContextAPI.jsx file

--- src/contextAPI/StoreContextAPI.jsx
+++ src/contextAPI/StoreContextAPI.jsx
@@ -1,11 +1,39 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import { fetchFoods, fetchMenus } from "../config/apiService";
 
 export const StoreContext = createContext(null);
 
 function StoreContextProvider(props) {
+  const [foodList, setFoodList] = useState([]);
+  const [menuList, setMenuList] = useState([]);
   const [cartItemsSelected, setCartItemsSelected] = useState({});
   const [purchaseHistory, setPurchaseHistory] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const foods = await fetchFoods();
+        const menus = await fetchMenus();
+
+        // Ensure that each item includes a valid image path
+        const updatedFoods = foods.map((food) => ({
+          ...food,
+          image: food.image || "/images/default_menu.png", // Provide a default image path if not available
+        }));
+
+        const updatedMenus = menus.map((menu) => ({
+          ...menu,
+          menu_image: menu.menu_image || "/images/default_menu.png", // Provide a default image path if not available
+        }));
+
+        setFoodList(updatedFoods);
+        setMenuList(updatedMenus);
+      } catch (error) {
+        console.error("Failed to load data", error);
+      }
+    };
+    loadData();
+  }, []);
 
   useEffect(() => {
     const storedHistory =
@@ -39,7 +67,7 @@ function StoreContextProvider(props) {
   function getTotalCartCost() {
     return Object.entries(cartItemsSelected).reduce(
       (totalCost, [itemId, quantity]) => {
-        const itemInformation = food_list.find(
+        const itemInformation = foodList.find(
           (product) => product._id === itemId
         );
         return (
@@ -51,7 +79,7 @@ function StoreContextProvider(props) {
   }
 
   function addPurchase(itemId, quantity) {
-    const itemInformation = food_list.find((product) => product._id === itemId);
+    const itemInformation = foodList.find((product) => product._id === itemId);
     if (itemInformation) {
       const newPurchase = {
         productName: itemInformation.name,
@@ -64,7 +92,8 @@ function StoreContextProvider(props) {
   }
 
   const contextValue = {
-    food_list,
+    foodList,
+    menuList,
     cartItemsSelected,
     setCartItemsSelected,
     addToCartSelected,
