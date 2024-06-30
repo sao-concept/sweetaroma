916b9fb701ac184688af7301d1c4c9de5393aa4b
Azeez Saka <saoconcept1@gmail.com>
2024-06-30 05:47:38 -0700

    Modified src/index.js file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
-// import StoreContextProvider from "./contextAPI/StoreContextAPI.jsx";
+import StoreContextProvider from "./contextAPI/StoreContextAPI.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    {/* <StoreContextProvider> */}
-    <App />
-    {/* </StoreContextProvider> */}
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
 );
